ceeb421bb01d6987d4e10c3d336d978f589a6666
Marina Privalova <privalovama@yandex.ru>
2023-07-10 16:00:40 +0300

    feat: currentUser, protectedRoute

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,50 +1,57 @@
-import React, { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Main from '../Main/Main';
-import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies';
-import Register from '../Register/Register';
-import Login from '../Login/Login';
-import Profile from '../Profile/Profile';
-import NotFound from '../NotFound/NotFound';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+import "./App.css";
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
+import Profile from "../Profile/Profile";
+import NotFound from "../NotFound/NotFound";
+
 
 function App() {
-
+  const [currentUser, setCurrentUser] = useState({});
   const [loggedIn, setLoggedIn] = useState(true);
 
- /**код для отрисовки карточек (без апи)*/ 
-  const cardsTotal = 16;
-  const cardsTotalSave = 4;
-  const cards = Array(cardsTotal).fill(null);
-  const saveCards = Array(cardsTotalSave).fill(null);
+  const [cards, setCards] = useState([]);
 
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<Main loggedIn={loggedIn} />} />
-        <Route path="/movies" 
-          element={
-            <Movies
-              loggedIn={loggedIn}
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Main loggedIn={loggedIn} />} />
+          <Route path="/movies"
+            element={<ProtectedRoute
+              element={Movies} 
+              loggedIn={loggedIn} 
               cards={cards}
             />
-          }
-        />
-        <Route path="/saved-movies" 
-          element={
-            <SavedMovies 
+            }
+          />
+          <Route path="/saved-movies"
+            element={<ProtectedRoute
+              element={SavedMovies} 
               loggedIn={loggedIn} 
-              cards={saveCards}
+              cards={cards} 
             />
-          } 
-        />
-        <Route path="/profile" element={<Profile loggedIn={loggedIn} />} />
-        <Route path="/signin" element={<Login />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="*"element={<NotFound />}/>
-      </Routes>
-    </div>
+            }
+          />
+          <Route path="/profile" 
+            element={<ProtectedRoute
+              element={Profile} 
+              loggedIn={loggedIn} 
+            />
+            } 
+          />
+          <Route path="/signin" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
